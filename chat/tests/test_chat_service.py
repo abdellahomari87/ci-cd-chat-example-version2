a20a5c2b86b917c99254c63d60084f3474635553
abdellahomari87 <152940131+abdellahomari87@users.noreply.github.com>
2024-09-30 14:47:45 +0200

    Update test_chat_service.py

--- chat/tests/test_chat_service.py
+++ chat/tests/test_chat_service.py
@@ -1,4 +1,4 @@
 from Chat.chat_service import get_message
 
 def test_get_message():
-    assert get_message() == "Hello to you from the Chat service! oui"
+    assert get_message() == "Hello to you from the Chat service! oui oui"
