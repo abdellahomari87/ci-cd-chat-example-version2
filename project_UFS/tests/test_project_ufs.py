5134e4921020dbe72618c14b01b27e85a3686006
abdellahomari87 <152940131+abdellahomari87@users.noreply.github.com>
2024-09-30 15:45:51 +0200

    Update test_project_ufs.py

--- project_UFS/tests/test_project_ufs.py
+++ project_UFS/tests/test_project_ufs.py
@@ -1,4 +1,4 @@
-from project_ufs.main import get_message  # Modifie l'import selon ta structure réelle
+from project_ufs.project_ufs_service import get_project_message
 
-def test_get_message():
-    assert get_message() == "Expected message from Project_UFS"
+def test_get_project_message():
+    assert get_project_message() == "Hello from the Project_UFS service!"
