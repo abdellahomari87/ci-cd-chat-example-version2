9b0679dd1e9458531743bd72887d845d443451b3
abdellahomari87 <152940131+abdellahomari87@users.noreply.github.com>
2024-09-30 16:17:36 +0200

    Update test_project_ufs.py

--- project_UFS/tests/test_project_ufs.py
+++ project_UFS/tests/test_project_ufs.py
@@ -1,4 +1,4 @@
-from project_ufs.project_ufs_service import get_project_message
+from project_UFS.project_ufs_service import get_project_message
 
 def test_get_project_message():
     assert get_project_message() == "Hello from the Project_UFS service!"
