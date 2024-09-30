25e68d3af3ec571d6fc8561c044d0c278aa2b6f1
abdellahomari87 <152940131+abdellahomari87@users.noreply.github.com>
2024-09-30 20:24:39 +0200

    Update test_chat_service.py

--- chat/tests/test_chat_service.py
+++ chat/tests/test_chat_service.py
@@ -1,5 +1,9 @@
+import unittest
 from chat.chat_service import get_message
 
+class TestChatService(unittest.TestCase):
+    def test_get_message(self):
+        self.assertEqual(get_message(), "Hello to you from the Chat service! oui oui")
 
-def test_get_message():
-    assert get_message() == "Hello to you from the Chat service! oui oui"
+if __name__ == '__main__':
+    unittest.main()
