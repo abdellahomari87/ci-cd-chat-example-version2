28f900945f6a3f72bf50df07e99f0140ee1aea18
abdellahomari87 <152940131+abdellahomari87@users.noreply.github.com>
2024-09-30 23:08:39 +0200

    Update test_chat_service.py

--- chat/tests/test_chat_service.py
+++ chat/tests/test_chat_service.py
@@ -4,6 +4,7 @@ from chat.chat_service import get_message
 
 class TestChatService(unittest.TestCase):
 
+    
     def test_get_message(self):
         self.assertEqual(get_message(), "Hello to you from the Chat service! oui oui")
 
